fa2b9dcd41d0b9648f19514807952e96d6765ddf
cmathgit <cruz.math@outlook.com>
2024-11-01 00:20:17 -0400

    added kjv verbatim script

--- generate_votd_verbatim_prompts_suno_ai.py
+++ generate_votd_verbatim_prompts_suno_ai.py
@@ -5,8 +5,37 @@ from datetime import timedelta
 from biblegateway_api import get_bible_votd_kjv #, get_bible_votd_amp, get_bible_votd_msg
 from write_to_file import check_and_write_to_file
 
-# Load Bible-kjv library
+# This file generates prompts for the Suno AI API using bible verses taken from the King James Version of the Bible verbatim to be used as lyrics. This is for custom prompts only.
 
+# Load Bible-kjv library
+with open('Bible-kjv/Books.json') as f:
+    bible_books = json.load(f)
+
+# choose a random bible book
+bible_book_choice = random.choice(bible_books)
+
+# choose a random chapter from the chosen bible book
+bible_chapter_choice = random.randint(1, bible_book_choice[1])
+
+# Load bible book and verses
+with open(f'Bible-kjv/{bible_book_choice[0]}.json') as f:
+    bible_verses = json.load(f)
+
+# Select the chosen chapter
+bible_verses_chapter = bible_verses["chapters"][bible_chapter_choice]
+
+# Get all verses for the chapter
+verses = bible_verses_chapter["verses"]
+
+# Initialize empty string to store all verses
+chapter_text = ""
+
+# Loop through all verses and concatenate their text
+for verse in verses:
+    chapter_text += verse["text"] + " "  # Add space between verses
+
+# Remove trailing space
+chapter_text = chapter_text.strip()
 
 # Load data from JSON files
 with open('lib/tempo_dict.json') as f:
@@ -52,60 +81,6 @@ with open('lib/concerto_instrument_family_keys_dict.json') as f:
     concerto_instrument_family_keys = json.load(f)
 
 # Function to generate a random biblical song prompt less than 200 characters in string length
-def generate_biblical_song_simple():
-    song_descr_prompt = ""
-    while True:
-        bible_song_genre_choice = random.choice(song_genre)
-        vocalist_choice = random.choice(vocalists)
-        instrument_choice_1 = random.choice(instruments)
-        instrument_choice_2 = random.choice(instruments)
-        bible_book_choice = random.choice(bible_chapters)
-        bible_chap_choice = random.randint(1, bible_book_choice[1])
-        tempo_choice = random.choice(tempos)
-        tempo_choice_int = random.randint(tempo_choice[1], tempo_choice[2])
-        musical_key_choice = random.choice(musical_keys)
-        
-        timestamp = datetime.now()
-        timestamp_str = timestamp.strftime('%Y%m%d_%H%M%S')
-        timestamp_plus_1_sec = (timestamp + timedelta(seconds=1)).strftime('%Y%m%d_%H%M%S')
-
-        yt_title_1 = f"Bible {bible_book_choice[0]} {bible_chap_choice} {bible_song_genre_choice[0]} {timestamp_str} [{musical_key_choice[0]}] [{tempo_choice[0]}] [AI Music]\n\n"
-        check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', yt_title_1)
-        print(yt_title_1)
-        print("")
-
-        yt_title_2 = f"Bible {bible_book_choice[0]} {bible_chap_choice} {bible_song_genre_choice[0]} {timestamp_plus_1_sec} [{musical_key_choice[0]}] [{tempo_choice[0]}] [AI Music]\n\n"
-        check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', yt_title_2)
-        print(yt_title_2)
-        print("")
-
-        suno_song_title = f"Bible {bible_book_choice[0]} {bible_chap_choice} {bible_song_genre_choice[0]} {tempo_choice[0]} {tempo_choice_int} {musical_key_choice[0]} {timestamp_str}\n\n"
-        suno_song_title_clean = suno_song_title.replace(",", "")
-        check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', suno_song_title_clean)
-        print(suno_song_title_clean)
-        print("")
-
-        suno_song_title_2 = f"Bible {bible_book_choice[0]} {bible_chap_choice} {bible_song_genre_choice[0]} {tempo_choice[0]} {tempo_choice_int} {musical_key_choice[0]} {timestamp_plus_1_sec}\n\n"
-        suno_song_title_clean_2 = suno_song_title_2.replace(",", "")
-        check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', suno_song_title_clean_2)
-        print(suno_song_title_clean_2)
-        print("")
-        
-        yt_descr = f"Using AI to generate biblically accurate {bible_song_genre_choice[0]} songs and music videos.\n\n"
-        check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', yt_descr)
-        print(yt_descr)
-        print("")
-        
-        song_descr_prompt = f'{bible_song_genre_choice[0]}, {vocalist_choice[0]} ({vocalist_choice[2]}), {instrument_choice_1}, {instrument_choice_2}, biblically inspired lyrics book of {bible_book_choice[0]} chapter {bible_chap_choice}, {tempo_choice[0]} {tempo_choice_int} bpm, {musical_key_choice[0]}'
-        
-        if len(song_descr_prompt) < 200:
-            check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', "Generated using Suno AI\n Suno_Link_Here\n\nStyle of Music Prompt:\n")
-            print("Generated using Suno AI\n Suno_Link_Here\n\nStyle of Music Prompt:")
-            break
-    
-    return song_descr_prompt
-    
-# Function to generate a random biblical song prompt less than 200 characters in string length
 def generate_biblical_song_custom():
     lyrics_prompt = ""
     style_of_music_prompt = ""
@@ -115,8 +90,6 @@ def generate_biblical_song_custom():
         instrument_choice_1 = random.choice(instruments)
         instrument_choice_2 = random.choice(instruments)
         instrument_choice_3 = random.choice(instruments)
-        bible_book_choice = random.choice(bible_chapters)
-        bible_chap_choice = random.randint(1, bible_book_choice[1])
         tempo_choice = random.choice(tempos)
         tempo_choice_int = random.randint(tempo_choice[1], tempo_choice[2])
         musical_key_choice = random.choice(musical_keys)
@@ -176,10 +149,7 @@ def generate_bible_gateway_votd_song_custom():
     instrument_choice_1 = random.choice(instruments)
     instrument_choice_2 = random.choice(instruments)
     instrument_choice_3 = random.choice(instruments)
-        
-    #bible_book_choice = random.choice(bible_chapters)
-    #bible_chap_choice = random.randint(1, bible_book_choice[1])
-        
+         
     # Use Bible Gateway Verse of the Day
     votds = get_bible_votd_kjv()
     print("Bible Verse Content: ", votds['votd']['content'])
@@ -333,19 +303,6 @@ def bible_gateway_votd_concerto_song_custom():
 exclude_styles_pop_genres_prompt = f'\n\nExclude Styles:\n{exclude_pop_genres[0]}, {exclude_pop_genres[1]}, {exclude_pop_genres[2]}, {exclude_pop_genres[3]}, {exclude_pop_genres[4]}, {exclude_pop_genres[5]}, {exclude_pop_genres[6]}, {exclude_pop_genres[7]}, {exclude_pop_genres[8]}, {exclude_pop_genres[9]}'
 
 new_prompt_div = "\n\n------------------------------New Prompt-------------------------------------\n"
-
-check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', new_prompt_div)
-print("\n------------------------------New Prompt-------------------------------------\n")
-# Generate and print the generate_biblical_song_simple string
-# print("Biblically inspired song prompt less than 200 characters in string length:")
-bible_song_prompt = generate_biblical_song_simple()
-check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', bible_song_prompt)
-print(bible_song_prompt)
-check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', exclude_styles_pop_genres_prompt)
-print(exclude_styles_pop_genres_prompt)
-check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', "\n\n Lyrics_Here")
-print("\n Lyrics_Here")
-check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', "\n\nOwnership and commercial use rights is retained for any songs generated by my self using Suno during active subscription, even after cancelling. Proof of ownership is available upon request. For more information, please reference Suno Knowledge Base Articles at https://help.suno.com/en/articles/2421505")
 
 check_and_write_to_file('log/bible_song_prompt_history.txt', 'bible_song_prompt_history', new_prompt_div)
 print("\n------------------------------New Prompt-------------------------------------\n")
